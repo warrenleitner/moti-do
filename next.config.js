85a5e3e00001c9930548addf45f4e13c464a2777
Warren Leitner <warren.t.leitner@gmail.com>
2025-03-28 00:21:56 -0700

    More attempted Vercel fixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Resolve case sensitivity issues for Vercel deployment
+    config.resolve.symlinks = false;
+    
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
