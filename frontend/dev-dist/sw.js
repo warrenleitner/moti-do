93a220da1da2b613c67435fb2c43a486d6d4ac81
Warren Leitner <warren.t.leitner@gmail.com>
2025-12-24 14:04:43 -0500

    feat(api): add database health check endpoint and corresponding tests

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-a87ad30a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.pck077jm55g"
+    "revision": "0.g9sk0upi1qs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
